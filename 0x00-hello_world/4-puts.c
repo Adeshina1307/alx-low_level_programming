508e4d34f793117eb30235bb3f1627ffe7c011d5
Ojomu Adeshina <samueladeshina24@gmail.com>
2022-09-08 20:13:15 -0700

    4-putts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
+* Description: using the main fuction
+*this program prints "programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
