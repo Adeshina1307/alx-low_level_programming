eb4d7aefec323894df5c279d83ff5fc7b2572f11
Ojomu Adeshina <samueladeshina24@gmail.com>
2022-09-10 04:49:18 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,16 +6,12 @@
  * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
-int main(void
+int main(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
+int n;
+for (n = '0'; n <= '9'; n++)
 {
-	putchar(c);
-}
-for (c = 'a'; c <= 'f'; c++)
-{
-	putchar(c);
+	putchar(n);
 }
 putchar('\n');
 return (0);
