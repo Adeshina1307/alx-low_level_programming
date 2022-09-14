037efc714985ad4e01c53f93ee775c355f8eed72
Ojomu Adeshina <samueladeshina24@gmail.com>
2022-09-14 08:44:11 -0700

    103-fibonacici.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,23 +1,25 @@
-#include <stdio.h>
+#include <stdio.h> 
+
 /**
-  *main - prints sum of even fibonacci
-  *suit elements
-  *fibonacci suit numbers
-  *Return: return 0
-  */
-int main(void)
-{
-unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
-while (n3 <= 4000000)
-{
-n3 = n1 + n2;
-n1 = n2;
-n2 = n3;
+ * main - prints the sum of all even Fibonacci numbers below 4,000,000 
+ *
+ * Return: Always 0. 
+ */
 
-if ((n1 % 2) == 0)
-sum += n1
-
+int main(void) 
+{
+long int total_sum, sum, first, second;
+total_sum = 0; 
+sum = 0; 
+first = 0; 
+second = 1; 
+while (sum < 4000000)
+{
+sum = first + second;
+if (sum % 2 == 0)
+{
+total_sum += sum;
 }
-printf("%ld\n", sum);
-return (0);
+first = second;
+second = sum;
 }
