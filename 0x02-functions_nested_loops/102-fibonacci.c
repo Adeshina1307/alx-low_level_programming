1676b70906e4efd00a942dc69eb404ad8f3bf601
Ojomu Adeshina <samueladeshina24@gmail.com>
2022-09-14 08:27:26 -0700

    102-fibonaccci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,6 @@
-#include <stdio.h> 
-
+#include <stdio.h>
 /** 
- * main - prints the first 50 Fibonacci numbers, starting with 1 and 2 
+ *main - prints the first 50 Fibonacci numbers, starting with 1 and 2 
  * Return: Always 0.
  */
 
@@ -12,7 +11,7 @@ long int first, second;
 int count;
 sum = 0;
 first = 0; 
-second = 1;
+second =1;
 count = 0;
 while (count < 49)
 {
